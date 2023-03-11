10ac491bcc611dc7d817d751e9579ee6b66e5fd8
Andrew Blakey <ablakey@gmail.com>
2023-03-11 16:16:18 -0500

    update

--- src/assets/sounds/index.ts
+++ src/assets/sounds/index.ts
@@ -7,12 +7,13 @@
 import bump from "./bump.mp3";
 import confirm from "./confirm.mp3";
 import crunch from "./crunch.mp3";
+import doorSlowOpen from "./doorSlowOpen.mp3";
 import explosion from "./explosion.mp3";
 import gun from "./gun.mp3";
 import laser from "./laser.mp3";
 import laugh from "./laugh.mp3";
 import slash from "./slash.mp3";
 
-export const sounds = { bump, confirm, crunch, explosion, gun, laser, laugh, slash };
+export const sounds = { bump, confirm, crunch, doorSlowOpen, explosion, gun, laser, laugh, slash };
 
 export type SoundName = keyof typeof sounds;
